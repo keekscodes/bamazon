8ad91dec9c6fc15bcc0eb6fb37c26c3de8e7121f
Kimberly Mueller <47461362+kamueller2@users.noreply.github.com>
2019-07-01 14:52:05 -0400

    Delete keys.js
    don't need file

--- keys.js
+++
@@ -1,9 +0,0 @@
-console.log('this is loaded');
-
-exports.connection = {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-};
