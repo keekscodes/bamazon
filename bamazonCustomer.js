5c975652c3b6016e175c73976c7051254f58e395
Kimberly <kamueller2@gmail.com>
2019-07-07 12:38:53 -0400

    fixed git error

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -35,133 +35,84 @@ connection.connect(function(error) {
     if (error) throw error;
     // console.log("connected as id " + connection.threadId + "\n");
     init();
-    makePurchase();
+    loadProducts();
     // connection.end();
 });
 
 let content = [];
 let contentQuantity = [];
 
-// function loadProducts() {
-//     connection.query('SELECT * FROM products', function(error, response) {
-//         var table = new Table({
-//             head: ['Item ID', 'Product Name', 'Department', 'Price', 'Quantity'],
-//             style: {
-//                 head: [] //disable colors in header cells
-//                     ,
-//                 border: [] //disable colors for the border
-//             },
-//             colWidths: [20, 45, 30, 12, 12] //set the widths of each column (optional)
-//         });
-
-//         response.forEach(function(row) {
-//             // var q = row.stock_quantity;
-//             let newRow = [row.id, row.product_name, row.department_name, "$" + row.price, row.stock_quantity]
-//             table.push(newRow)
-//             content.push(row.stock_quantity)
-//         })
-//         console.log(row.id)
-//         console.log(row.stock_quantity)
-//         console.log("\n" + table.toString());
-//         makePurchase();
-//     })
-// }
-
-function makePurchase() {
+function loadProducts() {
     connection.query('SELECT * FROM products', function(error, response) {
-            var table = new Table({
-                head: ['Item ID', 'Product Name', 'Department', 'Price', 'Quantity'],
-                style: {
-                    head: [] //disable colors in header cells
-                        ,
-                    border: [] //disable colors for the border
-                },
-                colWidths: [20, 45, 30, 12, 12] //set the widths of each column (optional)
-            });
-
-            response.forEach(function(row) {
-                let newRow = [row.id, row.product_name, row.department_name, "$" + row.price, row.stock_quantity]
-                table.push(newRow)
-                content.push(row.id);
-                contentQuantity.push(row.stock_quantity);
-            })
-            console.log("\n" + table.toString());
-        })
-        // ask user if they want to purchase something
-    inquirer
-        .prompt([
-
-            {
-                name: "purchase",
-                type: "list",
-                message: "Would you like to purchase an item?",
-                choices: ["Yes", "No"]
+        var table = new Table({
+            head: ['Item ID', 'Product Name', 'Department', 'Price', 'Quantity'],
+            style: {
+                head: [] //disable colors in header cells
+                    ,
+                border: [] //disable colors for the border
             },
+            colWidths: [20, 45, 30, 12, 12] //set the widths of each column (optional)
+        });
 
-        ]).then(function(answer) {
-            if (answer.purchase === "Yes") {
-                inquirer
-                    .prompt([{
-                            name: "id",
-                            type: "number",
-                            message: "What is the item ID of the product you would like to purchase?",
-                            validate: function(input) {
-                                if (Number.isInteger(parseInt(input)) === true && (content.indexOf(parseFloat(input)) !== -1)) {
-                                    return true;
-                                } else console.log("\nPlease enter a valid item ID to continue.")
-                            }
-                        }, {
-                            name: "quantity",
-                            type: "number",
-                            message: "How many would you like?",
-                            validate: function(input) {
-                                if (Number.isInteger(parseInt(input)) === true) {
-                                    return true
-                                } else console.log("\nPlease enter the number of units you would like to purchase.")
-                            }
-                        },
-
-                    ]).then(selection => {
-                        const product = parseInt(selection.id);
-                        const quantity = parseInt(selection.quantity);
-
-                        // place order for product with given id and given units to purchase
-                        placeOrder(product, quantity);
-                    });
-            }
-            //         ]).then(function(answer) {
-            //             var quantityNeeded = answer.quantity;
-            //             var IDrequested = answer.itemID;
-            //             // purchaseOrder(IDrequested, quantityNeeded)
-            //             // console.log(content);
-
-
-
-            //             if (contentQuantity[0] - quantityNeeded < 0) {
-            //                 console.log("insufficient quantity in our store");
-            //             } else if (contentQuantity[1] - quantityNeeded < 0) {
-            //                 console.log("insufficient quantity in our store");
-            //             } else {
-            //                 console.log("purchase successful")
-            //             }
-
-            //             connection.end();
-            //         })
-            // } else {
-            //     console.log('Thanks for visiting Bamazon');
-            //     connection.end();
-            // }
+        response.forEach(function(row) {
+            let newRow = [row.id, row.product_name, row.department_name, "$" + row.price, row.stock_quantity]
+            table.push(newRow)
+            content.push(row.stock_quantity)
         })
-
+        console.log("\n" + table.toString());
+        shop(response);
+    })
 }
 
-const placeOrder = function(product, quantity) {
-    // query database for product
-    connection.query(
-        "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?", [quantity, product.item_id],
-        function(error, response) {
-            console.log("Thank you for your purchase of " + quantity + " " + product + "'s!");
+function shop() {
+    inquirer.prompt([{
+            name: "id",
+            type: "input",
+            message: "Please enter the Item ID of the product you would like to purchase",
+            filter: Number
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "How many items would you like to purchase?",
+            filter: Number
+        },
 
-        }
-    )
-}
+    ]).then(function(answers) {
+        var quantityNeeded = answers.quantity;
+        var idReq = answers.id;
+        placeOrder(idReq, quantityNeeded);
+    });
+};
+
+
+function placeOrder(ID, amount) {
+    connection.query('Select * FROM products WHERE id = ' + ID, function(error, response) {
+        if (error) throw error;
+        if (amount <= response[0].stock_quantity) {
+            var totalCost = response[0].price * amount;
+            console.log("Good news your order is in stock!");
+            console.log("Your total cost for " + amount + " " + response[0].product_name + " is " + "$" + totalCost + " Thank you!");
+
+            connection.query("UPDATE products SET stock_quantity = stock_quantity - " + amount + "WHERE id = " + ID);
+        } else {
+            console.log("Insufficient quantity, sorry we do not have enough " + response[0].product_name + "to complete your order.");
+        };
+        shop();
+    });
+};
+
+// shop();
+
+
+
+// const placeOrder = function(product, quantity) {
+//     // query database for product
+//     connection.query(
+//         "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?", [quantity, product.item_id],
+//         function(error, response) {
+//             console.log("Thank you for your purchase of " + quantity + " " + selection.product_name + "'s!");
+
+//         }
+//     )
+// }
